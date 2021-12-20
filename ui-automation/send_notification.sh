09a54d96571cfd8a4536e7a146f3a186c1eb3156
KristineLegzdina <kristinelegzdina@KristindinasMBP.lan>
2021-12-20 18:03:16 +0200

    small changes

--- ui-automation/send_notification.sh
+++ ui-automation/send_notification.sh
@@ -14,7 +14,7 @@ fi
 
 # Send notification
 curl -X POST \
-  https://discordapp.com/api/webhooks/651765119101042689/JhoCn787dYW2s8c9CkqdYYizHTgUx9VT5pX65iCMfwYnC63C3Lc3tDGJsq0pnem3CD7B \
+  https://discord.com/api/webhooks/922206367115276368/jH9Mhwg2z8TwrS7ET6M2wnwZcSunDQhwMOYuj8Anjp93NWzCbKO6zWbMnXvMp94uX5rC \
   -H 'Cache-Control: no-cache' \
   -H 'Content-Type: application/json' \
   -d '{
